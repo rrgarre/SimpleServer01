cd32262a85f9c0cac619ec916e817fc61b70cdf8
rrgarre <rrgarre@gmail.com>
2023-03-11 19:34:54 +0100

    Aync en post users

--- controllers/users.js
+++ controllers/users.js
@@ -8,29 +8,60 @@ usersRouter.get('/', async (request, response)=>{
     return response.json(result)
 })
 
-usersRouter.post('/', (request, response)=>{
-    const body = request.body
+// usersRouter.post('/', (request, response)=>{
+//     const body = request.body
     
-    // bcrypt.hash(body.passwordHash, 10).then((currentHash)=>{
-    //     const user = new User({
-    //         username: body.username,
-    //         name: body.name,
-    //         passwordHash: currentHash
-    //     })
-    //     user.save()
-    //         .then(result => response.json(result))
-    // })
+//     // bcrypt.hash(body.passwordHash, 10).then((currentHash)=>{
+//     //     const user = new User({
+//     //         username: body.username,
+//     //         name: body.name,
+//     //         passwordHash: currentHash
+//     //     })
+//     //     user.save()
+//     //         .then(result => response.json(result))
+//     // })
 
-    const hash = bcrypt.hashSync(body.passwordHash, 10)
+//     const hash = bcrypt.hashSync(body.passwordHash, 10)
     
+//     const user = new User({
+//         username: body.username,
+//         name: body.name,
+//         passwordHash: hash
+//     })
+
+//     user.save()
+//         .then(result => response.json(result))
+    
+// })
+
+usersRouter.post('/', async (request, response)=>{
+    const body = request.body
+    const saltRounds = 10
+    
+    const hash = await bcrypt.hash(body.passwordHash, saltRounds)
+
+    console.log('XXXXX: ', hash)
+
     const user = new User({
         username: body.username,
         name: body.name,
         passwordHash: hash
     })
 
-    user.save()
-        .then(result => response.json(result))
+    savedUser = await user.save()
+    response.json(savedUser)
+    
+    //////////////// ESTA PARTE SIRVE SI NO ENTRAMOS EL EL CALLBACK DEL POST DE FORMA ASINCRONA
+    // const hash = bcrypt.hashSync(body.passwordHash, 10)
+    
+    // const user = new User({
+    //     username: body.username,
+    //     name: body.name,
+    //     passwordHash: hash
+    // })
+
+    // user.save()
+    //     .then(result => response.json(result))
     
 })
 
